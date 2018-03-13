fd356465b5f10ab4ff6769685bfee2b6feeceef6
AleViviana <alejandra_vcampos_97@hotmail.com>
2018-03-13 15:03:37 -0600

    se agrega formulario a la tabla proveedores

---
+++ postgres/ProveedoresForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace postgres
+{
+    public partial class ProveedoresForm : Form
+    {
+        public ProveedoresForm()
+        {
+            InitializeComponent();
+        }
+
+        private void proveedoresBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.proveedoresBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet);
+
+        }
+
+        private void ProveedoresForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet.proveedores' Puede moverla o quitarla según sea necesario.
+            this.proveedoresTableAdapter.Fill(this.postgresDataSet.proveedores);
+
+        }
+    }
+}
